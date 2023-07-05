2e95c2ab50c6ea17a41132f0a67044af3caa0e02
riskya3004 <90240113+riskya3004@users.noreply.github.com>
2023-07-05 08:06:48 +0700

    Revisi Navbar & ganti file FP Growth

--- resources/views/rfm/qq_frequency.blade.php
+++ resources/views/rfm/qq_frequency.blade.php
@@ -8,7 +8,7 @@
 
                  <h1 class="h3 mb-2 text-gray-800"QQ Plot RFM</h1>
 
-                <h5 class="h5 mb-2 text-gray-800 font-weight-bold">QQ_plot_Frequency</h5>
+                <h5 class="h5 mb-2 text-gray-800 font-weight-bold">QQ Plot Frequency</h5>
                 {{-- Frequency--}}
                 <img src="content/rfmanalis/1. QQ PLOT RFM/QQ_plot_Frequency.png" alt="" style="width: 900px">
                 <br>
--- resources/views/rfm/qq_recency.blade.php
+++ resources/views/rfm/qq_recency.blade.php
@@ -9,7 +9,7 @@
                  <h1 class="h3 mb-2 text-gray-800"QQ Plot RFM</h1>
 
                 {{-- Recency --}}
-                <h5 class="h5 mb-2 text-gray-800 font-weight-bold">QQ_plot_Recency</h5>
+                <h5 class="h5 mb-2 text-gray-800 font-weight-bold">QQ Plot Recency</h5>
                 <img src="content/rfmanalis/1. QQ PLOT RFM/QQ_plot_Recency.png" alt="" style="width: 900px">
 
                 </div>
